6ea5081f819282a868aa89189afdde749dead45a
jose <pucciarellijose@gmail.com>
2025-07-21 16:47:30 -0300

    implementing search and filtering

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -99,6 +99,8 @@ export const getCategories = async () => {
             appwriteConfig.databaseId,
             appwriteConfig.categoriesCollectionId
         )
+
+        return categories.documents;
     } catch (e) {
         throw new Error(e as string);
     }
